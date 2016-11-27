f2c69f500652a72b7bf2dc3aa279a26ee5c56cc8
richisgeo <richisgeo@gmail.com>
2016-11-27 12:19:05 -0500

    mod'd shapefile
    changed that

--- election_difference.R
+++ election_difference.R
@@ -1,7 +1,10 @@
-md <- pres_elect16_results[pres_elect16_results$st == 'MD',]
+library(reshape2)
+
+md <- pres.elect16.results[pres.elect16.results$st == 'MD',]
 md_clean <- na.omit(md) 
 md_clean
 names(md_clean)
 md2 <- dcast(md_clean,fips ~  cand, value.var="votes")
 names(md2)
 md2["v_diff"] <- md2$`Hillary Clinton` - md2$`Donald Trump`
+write.csv(md2, file = "md_elec.csv")
